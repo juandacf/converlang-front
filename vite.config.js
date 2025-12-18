dbcb0c4c6805e3dbecd4664a15a395e905b57c08
Juan Caballero <juandacf@gmail.com>
2025-12-18 18:08:07 -0500

    video llamada funcionando

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import fs from "fs";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    host: "0.0.0.0",
+    port: 5173,
+    https: {
+      key: fs.readFileSync("./localhost+2-key.pem"), //Estos nombres se deben actualizar al momento de la creación de los certificados
+      cert: fs.readFileSync("./localhost+2.pem"),
+    },
+  },
+});
